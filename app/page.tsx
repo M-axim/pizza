704437dad3a009f26912bbb8c4a38c3a459cd0af
maxim <isp2020boew@yandex.ru>
2026-04-13 23:41:56 +0300

    chainded page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello Next</h1>
+    <h1>
+      HOME
+      <Button variant="outline">Hello Next</Button> 
+    </h1>
   );
 }
